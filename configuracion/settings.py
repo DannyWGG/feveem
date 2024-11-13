0b0771bca006f01bafbc3333eda0f9564a335553
root <root@prueba-bots.mppe.gob.ve>
2024-11-13 10:55:00 -0400

    modificaciones en producción

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -12,8 +12,8 @@ import os
 BASE_DIR            = Path(__file__).resolve().parent.parent
 SECRET_KEY          = config('SECRET_KEY')
 DEBUG               = config('DEBUG')
-# ALLOWED_HOSTS     = config('ALLOWED_HOSTS', cast=Csv())
-ALLOWED_HOSTS       = [] 
+ALLOWED_HOSTS     = config('ALLOWED_HOSTS', cast=Csv())
+ 
 
 BASE_APPS   =   [
                     'jazzmin',
